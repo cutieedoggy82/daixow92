9cbca06329ab532ac1e0b90bb0083b16b0afe906
cutieedoggy82 <116865086+cutieedoggy82@users.noreply.github.com>
2023-08-17 18:17:53 +0800

    Update app.js

--- .netlify/functions/app.js
+++ .netlify/functions/app.js
@@ -1,39 +1,36 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const fs = require('fs');
+exports.handler = async (event, context) => {
+  const express = require('express');
+  const bodyParser = require('body-parser');
+  const fs = require('fs');
 
-const app = express();
-const port = 3000;
+  const app = express();
+  app.use(bodyParser.json());
 
-app.use(bodyParser.json());
+  const imageDataPath = './imageData.json';
 
-const imageDataPath = './imageData.json';
+  // Create a new image post
+  app.post('/post', (req, res) => {
+    const newImage = req.body.imageUrl;
 
-// Create a new image post
-app.post('/post', (req, res) => {
-  const newImage = req.body;
+    // Save the new image to JSON file
+    let imageData = [];
+    if (fs.existsSync(imageDataPath)) {
+      imageData = JSON.parse(fs.readFileSync(imageDataPath));
+    }
+    imageData.push({ imageUrl: newImage });
+    fs.writeFileSync(imageDataPath, JSON.stringify(imageData, null, 2));
 
-  // Save the new image to JSON file
-  let imageData = [];
-  if (fs.existsSync(imageDataPath)) {
-    imageData = JSON.parse(fs.readFileSync(imageDataPath));
-  }
-  imageData.push(newImage);
-  fs.writeFileSync(imageDataPath, JSON.stringify(imageData, null, 2));
+    res.send('(っ◕‿◕)っ♡ Image posted with love~ ♡');
+  });
 
-  res.send('(っ◕‿◕)っ♡ Image posted with love~ ♡');
-});
-
-// Get all images
-app.get('/images', (req, res) => {
-  if (fs.existsSync(imageDataPath)) {
-    const imageData = JSON.parse(fs.readFileSync(imageDataPath));
-    res.json(imageData);
-  } else {
-    res.json([]);
-  }
-});
-
-app.listen(port, () => {
-  console.log(`Your kawaii social network is running on port ${port}! (｡♥‿♥｡)`);
-});
+  return new Promise((resolve, reject) => {
+    app.listen(3000, () => {
+      console.log(`Your kawaii social network is running on port 3000! (｡♥‿♥｡)`);
+      resolve({
+        statusCode: 200,
+        body: 'Your kawaii social network is running on port 3000! (｡♥‿♥｡)'
+      });
+    });
+  });
+};
+                    
